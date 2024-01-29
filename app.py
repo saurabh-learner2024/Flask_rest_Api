ae8518e923b2acfa6db5c01444a93ae3c2a68d63
saurabh-learner2024 <saurabh.jaiswal0096@gmail.com>
2024-01-29 20:35:27 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,103 @@
+import os
+
+from blocklist import BLOCKLIST
+from resources.user import blp as UserBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.item import blp as ItemBlueprint
+from resources.tag import blp as TagBlueprint
+from flask import Flask, jsonify
+from flask_smorest import Api
+from db import db
+from flask_jwt_extended import JWTManager
+
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+
+    # if there is an exception that occurs hidden inside an extension
+    # of flask to propagate into main app
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+
+    # flask smorest configuration
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    # it tells flask-smorest to use swagger for the API documentation
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    api = Api(app)
+    app.config["JWT_SECRET_KEY"] = "83217045191784795094010035198527657082"
+    jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked."}
+            ),
+            401
+        )
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "This token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
+
+    return app
